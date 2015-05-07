48000c91836dd7d8c5d1df585e2fb1672a611661
bigjim1971 <bigjim1971@users.noreply.github.com>
2015-05-07 21:52:18 +0100

    Update first_class_function.py

--- first_class_function.py
+++ first_class_function.py
@@ -6,5 +6,9 @@ import math
 def hypo(x,y):
   return math.sqrt( (x*x) + (y*y) )
 
-result = math.trunc(hypo(3,4))
+def circleArea(r):
+  return math.pi*r*r
+
+# result = math.trunc(hypo(3,4))
+result = circleArea(3)
 print(result)
